9f7fafbc2f61d850fe8ff0dcb8a49e5056d66ca5
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-09-06 17:56:10 +0000

    container config done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,27 @@
+const accountId = 144553
+let accountEmail = "saqib@gmail.com"
+var accountPassword = "12345"
+accountCity = "Lahore" // never declare variable this way
+let accountState;
+
+// We can declare variables in two ways
+// 1) let 2) var
+// But we don't use var in js
+
+
+// accountId = 2 // Not Allowed
+
+accountEmail = "aly@gmail.com"
+accountPassword = "678910"
+accountCity = "Isb"
+
+console.log(accountId);
+
+/*
+    Prefer not to use var because of issue in block scope and functional scope
+*/
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
