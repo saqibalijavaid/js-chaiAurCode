2ecf84f68fa7e209e1d04a15b84946fa50862ec5
Saqib Ali Javaid <saqibalijavaid2@gmail.com>
2024-09-07 10:51:52 +0000

    dataTypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,36 @@
+ "use strict"; // treat all js code as newer js version
+
+// alert(3+3)// we are using nodejs, not browser
+
+
+// code readability should be high
+
+// console.log("Saqib"); console.log("Aly");
+
+console.log("Saqib")
+console.log("Aly")
+
+// Datatypes
+
+let name = "Saqib"
+let age = 18
+let isLoggedIn = false
+let state1;       // undefined
+let state2 = ""   // string
+let state3 = null // null
+
+// number -> 2 to power 53
+// bigint
+// string -> '' or ""
+// boolean -> True / False
+// null -> stand alone value
+// undefined -> value abhi assign nahi hoi hai
+// symbol -> unique - We will use it in React
+
+// object
+
+console.log(typeof "Saqib");
+console.log(typeof age);
+console.log(typeof 18);
+console.log(typeof null); // output will be -> object (Some people think its language error)
+console.log(typeof undefined); // output will be -> undefined
